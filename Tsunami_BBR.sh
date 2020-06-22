5f9ea7a1dba4a9e06d72c2158deff0c425baf089
Molly Lau <windowsbeta@outlook.com>
2020-06-22 21:59:40 +0800

    Update Tsunami_BBR.sh

--- Tsunami_BBR.sh
+++ Tsunami_BBR.sh
@@ -100,18 +100,20 @@ get_version(){
 
 get_url(){
 	get_version
-	#headers_all_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-headers" | awk -F'\">' '/all.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
-	#headers_all_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${headers_all_name}"
 	bit=`uname -m`
 	if [[ "${bit}" = "x86_64" ]]; then
 		image_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-image" | grep "lowlatency" | awk -F'\">' '/amd64.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
 		bit="amd64"
 		image_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${bit}/${image_name}"
+		headers_all_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-headers" | awk -F'\">' '/all.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
+		headers_all_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${bit}/${headers_all_name}"
 		headers_bit_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-headers" | grep "lowlatency" | awk -F'\">' '/amd64.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
 		headers_bit_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${bit}/${headers_bit_name}"
 	elif [[ "${bit}" = "i386" ]]; then
 		image_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-image" | grep "lowlatency" | awk -F'\">' '/i386.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
 		image_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${bit}/${image_name}"
+		headers_all_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-headers" | awk -F'\">' '/all.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
+		headers_all_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${bit}/${headers_all_name}"
 		headers_bit_name=`wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/ | grep "linux-headers" | grep "lowlatency" | awk -F'\">' '/i386.deb/{print $2}' | cut -d'<' -f1 | head -1 | cut -d'/' -f2`
 		headers_bit_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${required_version}/${bit}/${headers_bit_name}"
 	else
@@ -182,14 +184,14 @@ install_image(){
 }
 
 install_headers(){
-	#if [[ -f ${headers_all_name} ]]; then
-		 #echo -e "${Info} deb file exist"
-	#else echo -e "${Info} downloading headers_all" && wget ${headers_all_url}
-	#fi
-	#if [[ -f ${headers_all_name} ]]; then
-		 #echo -e "${Info} installing headers_all" && dpkg -i ${headers_all_name}
-	#else echo -e "${Error} headers_all download failed, please check !" && exit 1
-	#fi
+	if [[ -f ${headers_all_name} ]]; then
+		 echo -e "${Info} deb file exist"
+	else echo -e "${Info} downloading headers_all" && wget ${headers_all_url}
+	fi
+	if [[ -f ${headers_all_name} ]]; then
+		 echo -e "${Info} installing headers_all" && dpkg -i ${headers_all_name}
+	else echo -e "${Error} headers_all download failed, please check !" && exit 1
+	fi
 
 	if [[ -f ${headers_bit_name} ]]; then
 		 echo -e "${Info} deb file exist"
